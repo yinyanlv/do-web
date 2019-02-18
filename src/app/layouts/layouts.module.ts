57e1499426dd6042d8691c5f94c55815c0737ee0
yinyanlv <yanlv.yin@gmail.com>
2019-02-18 18:18:32 +0800

    add: full component

---
+++ src/app/layouts/layouts.module.ts
@@ -0,0 +1,12 @@
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {FullComponent} from './full/full.component';
+
+@NgModule({
+  declarations: [FullComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class LayoutsModule {
+}
