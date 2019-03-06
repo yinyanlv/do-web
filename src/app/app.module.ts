016815c4b3408ceb317de466a24946363ada6a00
yinyanlv <yanlv.yin@gmail.com>
2019-03-06 18:21:41 +0800

    chore: add some packages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import {BrowserModule} from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {NgModule} from '@angular/core';
 import {TestModule} from 'ngx-joy/test';
 import {ServicesModule} from 'ngx-joy/services';
@@ -13,6 +14,7 @@ import {LayoutsModule} from './layouts/layouts.module';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
     LayoutsModule,
     TestModule,
