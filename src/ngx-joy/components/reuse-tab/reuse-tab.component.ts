bd08957855e1884b03f1856f34d2dbbd6863ec91
yinyanlv <yanlv.yin@gmail.com>
2019-04-17 18:07:13 +0800

    add: reuse tab

---
+++ src/ngx-joy/components/reuse-tab/reuse-tab.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-reuse-tab',
+  templateUrl: './reuse-tab.component.html',
+  styleUrls: ['./reuse-tab.component.scss']
+})
+export class ReuseTabComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/ngx-joy/components/reuse-tab/reuse-tab.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReuseTabComponent } from './reuse-tab.component';
+
+@NgModule({
+  declarations: [ReuseTabComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class ReuseTabModule { }
