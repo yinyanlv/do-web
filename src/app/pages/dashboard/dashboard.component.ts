02b0daffb45be07761905ae413352f66a4d329b2
bugong <1761869346@qq.com>
2019-04-29 22:41:23 +0800

    edit: dashboard

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import {jAnimateChildStaggerAnimation, jNormalInAnimation} from 'src/ngx-joy/animations';
 
 @Component({
   selector: 'dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+  styleUrls: ['./dashboard.component.scss'],
+  animations: [jAnimateChildStaggerAnimation, jNormalInAnimation]
 })
 export class DashboardComponent implements OnInit {
 
